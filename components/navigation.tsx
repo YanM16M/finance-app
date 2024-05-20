113e7e6f730dd249c3f41eb46c6a2e63a0128ac1
Ging <YanM16M@gmail.com>
2024-05-20 19:23:21 +0200

    project done

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -28,10 +28,10 @@ const routes = [
         href: "/categories",
         label: "Categories",
     },
-    {
-        href: "/settings",
-        label: "Settings",
-    },
+    // {
+    //     href: "/settings",
+    //     label: "Settings",
+    // },
 ];
 
 export const Navigation = () => {
